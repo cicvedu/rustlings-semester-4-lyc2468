80a746d0d4950c1c3fa640d741e67e8cdabd7c04
skies <709029461@qq.com>
2024-07-06 15:26:22 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,10 +32,15 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(&str, Command)>) -> Vec<&str> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<&str> = vec![];
         for (string, command) in input.iter() {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase().as_str()),
+                Command::Trim => output.push(string.trim()),
+                Command::Append(times) => output.push("bar".repeat(*times).as_mut_str()),
+            }
             // TODO: Complete the function body. You can do it!
         }
         output
@@ -45,7 +50,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
