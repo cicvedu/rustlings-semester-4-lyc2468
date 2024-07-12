ed9f1b802157c1e4e51685fea1d235c106848328
sky <709029461@qq.com>
2024-07-12 09:50:51 +0800

    Update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,26 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red = tuple.0.try_into();
+        if red.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let green = tuple.1.try_into();
+        if green.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let blue = tuple.2.try_into();
+        if blue.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Self {
+            red: red.unwrap(),
+            green: green.unwrap(),
+            blue: blue.unwrap(),
+        })
     }
 }
 
@@ -48,6 +66,26 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = arr[0].try_into();
+        if red.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let green = arr[1].try_into();
+        if green.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let blue = arr[2].try_into();
+        if blue.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Self {
+            red: red.unwrap(),
+            green: green.unwrap(),
+            blue: blue.unwrap(),
+        })
     }
 }
 
@@ -55,6 +93,30 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let red = slice[0].try_into();
+        if red.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let green = slice[1].try_into();
+        if green.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let blue = slice[2].try_into();
+        if blue.is_err() {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Self {
+            red: red.unwrap(),
+            green: green.unwrap(),
+            blue: blue.unwrap(),
+        })
     }
 }
 
